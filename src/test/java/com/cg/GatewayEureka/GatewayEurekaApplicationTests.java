669bfa80bd9572c433becf9e38db9fa5c3da0124
sravanidevarapalli <sravanidevarapalli17@gmail.com>
2021-05-06 20:22:57 +0530

    first commit

---
+++ src/test/java/com/cg/GatewayEureka/GatewayEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.GatewayEureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
